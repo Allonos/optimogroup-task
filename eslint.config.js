82e6e179e055258d3c884e9c5ab010f91217f7c3
Revaz Tsitsvidze <revaztsitsvidze@Revazs-MacBook-Pro.local>
2026-07-01 12:37:52 +0400

    feat: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import unusedImports from "eslint-plugin-unused-imports";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
+import importPlugin from "eslint-plugin-import";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      js.configs.recommended,
+      react.configs.flat.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    plugins: {
+      react,
+      "unused-imports": unusedImports,
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      "prettier/prettier": ["warn", { singleQuote: true, jsxSingleQuote: true }],
+      "unused-imports/no-unused-imports": "error",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "import/order": ["warn", { "newlines-between": "always" }],
+    },
+  },
+]);
